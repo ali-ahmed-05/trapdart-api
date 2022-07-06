9c0aca3c623ccfd32d5adda344eb0378b672f1c4
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2022-07-06 22:57:18 +0500

    asd

--- package/addresses.js
+++ package/addresses.js
@@ -1,6 +1,6 @@
 
- export const crowdsale_addr = "0xb8223ABb74484E2517890cb49dA2dBCdB2035cE8"
- export const trapDart_addr = "0x19C7b2cC1FD0aD82Cf83bFe8ba0CaC77c772C228"
- export const vesting_addr = "0x1c59B31AA042D40Ac3C9Fa4f1e9CD6255c8d41bF"
- export const nFT_addr = "0xF15322831b9b7A1b4fF15b7a90544249df0d0d60"
+ const crowdsale_addr = "0xb8223ABb74484E2517890cb49dA2dBCdB2035cE8"
+ const trapDart_addr = "0x19C7b2cC1FD0aD82Cf83bFe8ba0CaC77c772C228"
+ const vesting_addr = "0x1c59B31AA042D40Ac3C9Fa4f1e9CD6255c8d41bF"
+ const nFT_addr = "0xF15322831b9b7A1b4fF15b7a90544249df0d0d60"
  module.exports = {trapDart_addr}
