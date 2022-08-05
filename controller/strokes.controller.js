c36c2a43270c7fb6966d66daae8b7e276f93ffc5
JunaidSikander <junaid_sikander@live.com>
2022-08-05 06:32:18 +0500

    [ADD] get proposals by stroke

--- controller/strokes.controller.js
+++ controller/strokes.controller.js
@@ -1,12 +1,11 @@
-
 const pool = require("../db/pool");
 
 //@desc     create stroke
 //@route    POST /api/strokes
 //@access   Public
 
-const createStroke =  async (req, res) => {
-    try{
+const createStroke = async (req, res) => {
+    try {
         const {proposal_ids} = req.body;
 
         console.log(proposal_ids);
@@ -16,13 +15,13 @@ const createStroke =  async (req, res) => {
 
         const data = await pool.query("INSERT INTO strokes (proposal_ids) VALUES($1)  RETURNING *", [ids]);
 
-        if(data.rowCount > 0) {
+        if (data.rowCount > 0) {
             res.status(200).send({status: true, message: 'stoke successfully created'});
-        }else {
+        } else {
             res.status(200).send({status: true, message: 'invalid input'});
         }
 
-    }catch (e) {
+    } catch (e) {
         res.status(400).send({status: false, message: e.message});
         console.log(e);
     }
@@ -33,8 +32,8 @@ const createStroke =  async (req, res) => {
 //@route    PUT /api/strokes
 //@access   Public
 
-const updateStroke =  async (req, res) => {
-    try{
+const updateStroke = async (req, res) => {
+    try {
         const {proposal_ids} = req.body;
         const {id} = req.params;
 
@@ -42,14 +41,14 @@ const updateStroke =  async (req, res) => {
 
         const ids = JSON.stringify(proposal_ids);
 
-        const data = await pool.query("UPDATE strokes SET proposal_ids = $1 WHERE id =$2 RETURNING *", [ids ,id]);
+        const data = await pool.query("UPDATE strokes SET proposal_ids = $1 WHERE id =$2 RETURNING *", [ids, id]);
 
-        if(data.rowCount > 0) {
+        if (data.rowCount > 0) {
             res.status(200).send({status: true, message: 'stoke successfully Updated'});
-        }else {
+        } else {
             res.status(200).send({status: true, message: 'invalid input'});
         }
-    }catch (e) {
+    } catch (e) {
         res.status(400).send({status: false, message: e.message});
         console.log(e);
     }
@@ -59,15 +58,39 @@ const updateStroke =  async (req, res) => {
 //@route    GET /api/strokes
 //@access   Public
 
-const getAllStrokes = async (req,res) => {
+const getAllStrokes = async (req, res) => {
     try {
         const records = await pool.query('SELECT * FROM strokes');
         console.log(records);
         res.status(200).send(records.rows);
-    }catch (e) {
+    } catch (e) {
         res.status(400).send({status: false, message: e.message});
         console.log(e);
     }
 }
 
-module.exports = {createStroke,getAllStrokes,updateStroke}
+//@desc     get proposals by stroke
+//@route    GET /api/strokes/:id
+//@access   Public
+
+const getProposalsByStroke = async (req, res) => {
+    try{
+        const {id} = req.params;
+
+        let proposalIds = await pool.query('SELECT proposal_ids FROM strokes WHERE id = $1', [id]);
+        proposalIds = proposalIds.rows[0].proposal_ids;
+
+        proposalIds = proposalIds.map(id => `'${id}'`).join(',')
+
+        const records = await pool.query(`SELECT * FROM proposals WHERE id IN (${proposalIds})`);
+        const proposals = records.rows;
+
+
+        res.status(200).send(proposals);
+    }catch (e) {
+        res.status(400).send({status: false, message: e.message});
+        console.log(e.message)
+    }
+}
+
+module.exports = {createStroke, getAllStrokes, updateStroke, getProposalsByStroke}
