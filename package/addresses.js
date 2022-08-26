857ebdedecd4ed97efca55c4fce1d811bb632483
ali <aliahmed.pluton@gmail.com>
2022-08-26 21:34:48 +0500

    updated

--- package/addresses.js
+++ package/addresses.js
@@ -1,10 +1,10 @@
 
- const crowdsale_addr = "0xc7aCd50dfA1D89213bdB0742E46029Fa2343Eb30"
- const trapDart_addr = "0x0011635A2339770a371c7291c2c6c463974b497e"
- const vesting_addr = "0xFB20014f434b66c88dad6C09fB3E14B7BB318658"
- const nFT_addr = "0xc6EAeCa60c4716EfD2A2B28A233533a3153E4447"
- const Vesting_dev_addr = "0xC1b94D303a19f7F11F8eB2E977D7eA826146E529"
- const Vesting_treasury_addr = "0x9d6FB46b35e7116901fF736FA40bFD8e8D9bFf74"
- const Vesting_founder_addr = "0x2c636cDa1F01b4cF98900b48ffE1A8E07a74e0ff"
+ const crowdsale_addr = "0x93da99aDd70cB6B5Ce1e312f9508002C77BB30cC"
+ const trapDart_addr = "0x72d70A89Edd37013f4de21210BD30DdC8f60C86B"
+ const vesting_addr = "0x055c16C18D5c2FAc6aFd156173A46C07d4Cf357F"
+ const nFT_addr = "0x503Eda05283B31bcF374478a4c3D408C4f5A1714"
+ const Vesting_dev_addr = "0x60E0e03985df00fdE649d3668606Bee00dA0e4C5"
+ const Vesting_treasury_addr = "0x0860267d0753Ef1aFf41f989537Be8246ad01039"
+ const Vesting_founder_addr = "0x81aacE711414E16302077472514C84F16566C6ee"
  
- module.exports = {trapDart_addr}
+module.exports = {trapDart_addr}
