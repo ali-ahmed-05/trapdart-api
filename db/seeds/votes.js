3f77269b31275d40297261fbbc87a5021e9e720e
ali <aliahmed.pluton@gmail.com>
2022-08-26 19:49:04 +0500

    updated

--- db/seeds/votes.js
+++ db/seeds/votes.js
@@ -3,19 +3,19 @@ const VotesModel = require('../models/votes.model');
 
 exports.seed = async function (knex) {
     await knex(VotesModel.table).del()
-    await knex(VotesModel.table).insert([
-        {
-            voter_address: 'some_random_text 1',
-            selected_option: 'option 1',
-            proposals_id: 0
-        }, {
-            voter_address: 'some_random_text 2',
-            selected_option: 'option 2',
-            proposals_id: 1
-        },
-        {
-            voter_address: 'some_random_text 3',
-            selected_option: 'option 3',
-            proposals_id: 2
-        },]);
+    // await knex(VotesModel.table).insert([
+    //     {
+    //         voter_address: 'some_random_text 1',
+    //         selected_option: 'option 1',
+    //         proposals_id: 0
+    //     }, {
+    //         voter_address: 'some_random_text 2',
+    //         selected_option: 'option 2',
+    //         proposals_id: 1
+    //     },
+    //     {
+    //         voter_address: 'some_random_text 3',
+    //         selected_option: 'option 3',
+    //         proposals_id: 2
+    //     },]);
 }
