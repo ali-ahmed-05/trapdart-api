81fbd305af6dea270e0d163e6947fa04a6fef38f
ali <aliahmed.pluton@gmail.com>
2022-08-26 19:52:05 +0500

    updated

--- db/seeds/proposals.js
+++ db/seeds/proposals.js
@@ -3,33 +3,33 @@ const ProposalModel = require('../models/proposals.model');
 
 exports.seed = async function (knex) {
     await knex(ProposalModel.table).del()
-    await knex(ProposalModel.table).insert([
-        {
-            title: 'some_random_text 1',
-            description: 'description 1',
-            image: 'image 1',
-            vote_type: 'NORMAL',
-            options: '{"5%":0,"10%":0,"15%":0,"20%":0}',
-            closing_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
-            total_votes: 0
-        },
-        {
-            title: 'some_random_text 2',
-            description: 'description 2',
-            image: 'image 2',
-            vote_type: 'NFT',
-            options: '{"5%":0,"10%":0,"15%":0,"20%":0}',
-            closing_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
-            total_votes: 1
-        },
-        {
-            title: 'some_random_text 3',
-            description: 'description 3',
-            image: 'image 3',
-            vote_type: 'NORMAL',
-            options: '{"5%":0,"10%":0,"15%":0,"20%":0}',
-            closing_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
-            total_votes: 2
-        },
-    ]);
+    // await knex(ProposalModel.table).insert([
+    //     {
+    //         title: 'some_random_text 1',
+    //         description: 'description 1',
+    //         image: 'image 1',
+    //         vote_type: 'NORMAL',
+    //         options: '{"5%":0,"10%":0,"15%":0,"20%":0}',
+    //         closing_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
+    //         total_votes: 0
+    //     },
+    //     {
+    //         title: 'some_random_text 2',
+    //         description: 'description 2',
+    //         image: 'image 2',
+    //         vote_type: 'NFT',
+    //         options: '{"5%":0,"10%":0,"15%":0,"20%":0}',
+    //         closing_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
+    //         total_votes: 1
+    //     },
+    //     {
+    //         title: 'some_random_text 3',
+    //         description: 'description 3',
+    //         image: 'image 3',
+    //         vote_type: 'NORMAL',
+    //         options: '{"5%":0,"10%":0,"15%":0,"20%":0}',
+    //         closing_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
+    //         total_votes: 2
+    //     },
+    // ]);
 }
--- db/seeds/strokes.js
+++ db/seeds/strokes.js
@@ -2,9 +2,9 @@ const strokesModel = require('../models/strokes.model');
 
 exports.seed = async function (knex) {
     await knex(strokesModel.table).del()
-    await knex(strokesModel.table).insert([
-        {proposal_ids: null},
-        {proposal_ids: null},
-        {proposal_ids: null},
-    ]);
+    // await knex(strokesModel.table).insert([
+    //     {proposal_ids: null},
+    //     {proposal_ids: null},
+    //     {proposal_ids: null},
+    // ]);
 }
